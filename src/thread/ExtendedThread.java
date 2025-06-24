610990493e8c7056a289ece5bdd860c994a83059
91801 <91801@naveen>
2025-06-24 20:43:39 +0530

    update

--- src/thread/ExtendedThread.java
+++ src/thread/ExtendedThread.java
@@ -10,30 +10,3 @@ public class ExtendedThread extends Thread  {
 
     }
 }
-
-
-/*package thread;
-
-public class ExtendedThread extends Thread {
-    
-    public ExtendedThread(String name) {
-        super(name); // Set custom thread name using superclass constructor
-    }
-
-    @Override
-    public void run() {
-    	
-        System.out.println("Inside run() -> Thread Name: " + this.getName() +", Priority: " + this.getPriority() +", State: " + this.getState());
-
-
-        System.out.println("Going to sleep: " + getName());
-        try {
-            Thread.sleep(60000);
-        } catch (InterruptedException e) {
-            System.out.println(getName() + " was interrupted during sleep.");
-        }
-
-        System.out.println("After sleeping: " + getName());
-    }
-}
-*/
